696b11b1ddc3df6537c9c5a50ba3971641f757cd
chusen <chusen12@163.com>
2021-03-13 17:04:40 +0800

    feat: 添加商品流程，验证库存 分类

--- src/case/test_02_product/test_01_product_create.py
+++ src/case/test_02_product/test_01_product_create.py
@@ -5,6 +5,7 @@ import random
 import pytest
 
 from src.api import category, product, base, supplier
+from src.db import product_repository
 from src.common.json_util import OperationJson
 from src.config.readConfig import ReadConfig
 
@@ -37,8 +38,10 @@ class TestProductCreate(object):
         """
         logger.info("test_01_create_product_without_inventory")
         # 组织数据
+        category_id = category.choice_random_second_category()['id']
+
         product_data = self.json.get_data('create_product_without_inventory')
-        product_data['categoryId'] = category.choice_random_second_category()['id']
+        product_data['categoryId'] = category_id
         product_data['name'] = product_data['name'] + str(random.randint(0, 1000))
         product_data['unit'] = base.random_unit()
         product_data['salePrice'] = base.random_price()
@@ -49,6 +52,9 @@ class TestProductCreate(object):
         # 验证
         assert resp['code'] == 200, resp['message']
         # TODO 验证商品其他信息，例如库存。 通过查询数据库的方式验证数字是否正确
+        # 验证店铺分类是否已经存在了
+        store_category_list = product_repository.get_store_category(self.store_id)
+        assert category_id in store_category_list, '店铺未关联上分类'
 
     def test_01_create_product_with_inventory(self) -> None:
         """
@@ -58,21 +64,25 @@ class TestProductCreate(object):
 
         logger.info("test_01_create_product_with_inventory")
         # 组织数据
+        inventory_quantity = base.random_number()
         product_data = self.json.get_data('create_product_with_inventory')
         product_data['categoryId'] = category.choice_random_second_category()['id']
         product_data['name'] = product_data['name'] + str(random.randint(0, 1000))
         product_data['unit'] = base.random_unit()
         product_data['salePrice'] = base.random_price()
         product_data['purchasePrice'] = base.random_price()
-        product_data['inventoryQuantity'] = base.random_number()  # 库存数量
-        product_data['supplierId'] = supplier.random_supplier(self.store_id)['supplierId']
-
+        product_data['inventoryQuantity'] = inventory_quantity  # 库存数量
+        product_data['supplierName'] = supplier.random_supplier(self.store_id)['name']
         # 调用接口
         resp = product.product_create(self.store_id, product_data)
-
+        # 接口返回的商品编码
+        prod_code = resp['data']
         # 验证
         assert resp['code'] == 200, resp['message']
         # TODO 验证商品其他信息，例如库存。 通过查询数据库的方式验证数字是否正确
+        # 验证库存
+        inventory_quantity_from_db = product_repository.get_product_inventory_quantity(self.store_id, prod_code)
+        assert inventory_quantity_from_db == inventory_quantity, '库存数量不匹配'
 
 
 if __name__ == "__main__":
